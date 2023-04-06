2e868c4ffce9d32262d8f65a313f84b28380f6e2
Rus <iYablokoG@gmail.com>
2023-04-06 13:22:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,7 +14,7 @@ def solution(p: float, x: np.array) -> tuple:
     z1 = chi2.ppf(alpha/2,df=2*n)
     z2 = chi2.ppf(1-alpha/2,df=2*n)
     
-    right = np.sqrt(sum(x**2)/(z1*47))
-    left = np.sqrt(sum(x**2)/(z2*47))
+    right = np.sqrt(sum(x**2)/(z1*24))
+    left = np.sqrt(sum(x**2)/(z2*24))
     
     return left, right
